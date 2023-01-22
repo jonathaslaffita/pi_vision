c8495219e11b44d2af4bf7fb6595dd3c957007df
jonathaslaffita <your>
2023-01-22 22:04:17 +0100

    distance works

---
+++ CMakeFiles/Distance_good.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Distance_good.dir/distance_good.cpp.o"
+  "Distance_good"
+  "Distance_good.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Distance_good.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
